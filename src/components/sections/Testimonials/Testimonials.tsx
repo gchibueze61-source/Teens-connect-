7deaf597e86d2b800a7303ec1030239971e3aafc
unknown <gchibueze61@gmail.com>
2026-08-22 07:18:35 +0100

    Add testimonial image

--- src/components/sections/Testimonials/Testimonials.tsx
+++ src/components/sections/Testimonials/Testimonials.tsx
@@ -5,7 +5,7 @@ const testimonials = [
     id: 1,
     name: "Emmanuel Sadiq",
     role: " Graphic designer and Teen Member",
-    image: "src/components/sections/Hero/207574 (2).jpg",
+    image: "src/components/sections/Testimonials/207574 (2).jpg",
     rating: 5,
     message:
       "Teens Connect Africa gave me the confidence to speak in public and believe in my future. Every meeting leaves me motivated to become a better leader."
@@ -15,7 +15,7 @@ const testimonials = [
     id: 2,
     name: "Olaniyi Shakira",
     role: "President of TCA 2026,spoken Word Poet",
-    image: "src/components/sections/Hero/aa019b45-3609-411b-97d5-d825f8e4bd77 (1).jpg",
+    image: "src/components/sections/Testimonials/aa019b45-3609-411b-97d5-d825f8e4bd77 (1).jpg",
     rating: 5,
     message:
       "I joined because I wanted friends, but I found mentors, opportunities and a family that truly believes in young people. I am ecited i have discovered my public speaking skills."
@@ -25,7 +25,7 @@ const testimonials = [
     id: 3,
     name: "Obize Gospel",
     role: "Tech Enthusiast and Teen Member",
-    image: "src/components/sections/Hero/126908.jpg",
+    image: "src/components/sections/Testimonials/126908.jpg",
     rating: 5,
     message:
       "I started with this organization from the beginning. The programs and leadership trainings helped me discover talents I never knew I had. My confidence has grown tremendously."
@@ -35,7 +35,7 @@ const testimonials = [
     id: 4,
     name: "Success Sadiq",
     role: "Teen Member",
-    image: "src/components/sections/Hero/207577 (1).jpg",
+    image: "src/components/sections/Testimonials/207577 (1).jpg",
     rating: 5,
     message:
       "Every monthly meeting teaches me something new. I've learned communication, teamwork and how to set meaningful goals."
@@ -45,7 +45,7 @@ const testimonials = [
     id: 5,
     name: "Anslem Chiagozie",
     role: "Spoken Word Poet and Teen Member",
-    image: "src/components/sections/Hero/207578 (1) (1).jpg",
+    image: "src/components/sections/Testimonials/207578 (1) (1).jpg",
     rating: 5,
     message:
       "Being part of this community has inspired me to pursue public speaking. I now believe I can make a difference in Africa."
@@ -55,7 +55,7 @@ const testimonials = [
     id: 6,
     name: "Abraham O.",
     role: "Auto engineer and Teen Member",
-    image: "src/components/sections/Hero/207566 (1).jpg",
+    image: "src/components/sections/Testimonials/207566 (1).jpg",
     rating: 5,
     message:
       "Teens Connect Africa has changed how I see myself. I've made lifelong friends and gained skills that will help me throughout my life."
